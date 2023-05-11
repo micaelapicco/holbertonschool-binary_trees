f5720c73efc3e65b88a64cd27c8f9a59e596751b
micaelapicco <micaelaabril421@gmail.com>
2023-05-11 10:53:59 -0700

    add main, header, and test 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - create a binary tree node
+ * @parent: parent node
+ * @value: content of node
+ * Return: new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    
+}
