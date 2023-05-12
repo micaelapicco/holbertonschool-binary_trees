48f646a2531b633202291c221a9565970548b9b1
micaelapicco <micaelaabril421@gmail.com>
2023-05-12 08:49:46 -0700

    add left node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,11 +13,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 
-	new = malloc(sizeof(binary_tree_t));
-	if (!new)
-		return (NULL);
-
 	new = binary_tree_node(parent, value);
+	
 	if (parent->left == NULL)
 		parent->left = new;
 
