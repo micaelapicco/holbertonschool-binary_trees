efb1a8e7418ff30b1e97bac4c9cf7adf0731ad63
micaelapicco <micaelaabril421@gmail.com>
2023-05-12 08:43:02 -0700

    add left node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -26,6 +26,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		new->left = parent->left;
 		parent->left = new;
 		new->left->parent = new;
+		new->right = NULL;
 	}
 	return (new);
 }
