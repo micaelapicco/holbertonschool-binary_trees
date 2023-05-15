15b4bc1c5825a528b6d678fed9f374949aae193f
micaelaapicco <micaelaaril421@gmail.com>
2023-05-15 12:15:55 -0700

    aa

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,8 +12,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	}
 	if (binary_tree_balance(tree) && binary_tree_height(tree))
-
-	return (0);
+		{
+			return (0);
+		}
 	return (1);
 
 }
