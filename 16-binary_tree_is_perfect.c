0f23c2904245265d298703e706ee5fc0dbed8459
micaelaapicco <micaelaaril421@gmail.com>
2023-05-15 12:28:18 -0700

    mal

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -13,9 +13,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	}
 	if (binary_tree_is_full(tree) && binary_tree_balance(tree) == 0)
 	{
-		return (0);
+		return (1);
 	}
-	return (1);
+	return (0);
 
 }
 
